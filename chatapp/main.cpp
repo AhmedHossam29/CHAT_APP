385689c587706d0976bec25e52937a64a2d71a54
AhmedAshraf780 <amedashraf780@gmail.com>
2025-04-17 20:00:14 +0200

    first commit

--- chatapp/main.cpp
+++ chatapp/main.cpp
@@ -1,10 +1,6 @@
-#include "chatapp.h"
 #include <QtWidgets/QApplication>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    chatapp w;
-    w.show();
     return a.exec();
 }
